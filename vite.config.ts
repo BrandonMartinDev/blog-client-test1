e49b4c44658a118dcfb9ed4e7542bc60d2087010
Brandon Martin <SomeBusinessEmail@protonmail.com>
2024-08-01 17:01:11 -0400

    Added path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+import path from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@config": path.resolve(__dirname, "./src/config"),
+      "@global-components": path.resolve(__dirname, "./src/components"),
+      "@layouts": path.resolve(__dirname, "./src/layouts"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@custom-types": path.resolve(__dirname, "./src/types")
+    }
+  }
 })
