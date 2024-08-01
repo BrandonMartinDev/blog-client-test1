0ba8b1f6493f9dc161d4f654b0dba7028a006d0b
Brandon Martin <SomeBusinessEmail@protonmail.com>
2024-08-01 17:02:58 -0400

    Created useGetUserInfo hook

---
+++ src/hooks/useGetUserInfo.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+
+import { API_URL } from "@config/defaults";
+import { type User } from "@custom-types/user-types";
+
+const ENDPOINT: string = `${API_URL}/user/`;
+
+
+
+export default function useGetUserInfo(user_id: string) {
+
+    if (!user_id) throw new Error("'user_id' was not provided!");
+
+
+    // Sets up userInfo state
+
+    const [userInfo, setUserInfo] = useState<User>();
+    const [err, setErr] = useState<unknown>();
+
+
+    // Creates useEffect that depends on user_id
+
+    useEffect(() => {
+
+        (async () => {
+
+            try {
+
+                const response = await fetch(`${ENDPOINT}${user_id}`);
+                const parsedResponse = await response.json();
+                if (!parsedResponse || !("data" in parsedResponse) || !("message" in parsedResponse)) throw new Error("There was an error getting the data");
+
+                setUserInfo(parsedResponse.data);
+
+                console.log(parsedResponse.message, parsedResponse.data);
+
+            } catch (err) {
+                setErr(err);
+            }
+
+        })();
+
+    }, [user_id]);
+
+
+    // Returns userInfo
+
+    return {
+        userInfo,
+        err
+    };
+
+}
