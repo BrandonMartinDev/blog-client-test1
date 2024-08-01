859886f0521527db106d7c940453ebd1a5aad513
Brandon Martin <SomeBusinessEmail@protonmail.com>
2024-08-01 19:41:56 -0400

    Created basic home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,84 @@
-const HomePage = () => {
+// -- == [[ IMPORTS ]] == -- \\
+
+import './homePage.css';
+
+import useGetFeaturedBlogs from '@hooks/useGetFeaturedBlogs';
+
+import { Article } from '@custom-types/blog-types';
+
+import {
+    MainArticlePreview,
+    ArticlePreview
+} from "@global-components/exportGlobalComponents.ts";
+
+
+
+// -- == [[ COMPONENTS ]] == -- \\
+
+const HomePageError = () => {
     return (
-        <main className="home">
-            <h1>Home Page</h1>
-        </main>
+        <div className="container">
+            <main className="home-main">
+                <p>Home</p>
+                <h1>There was an error loading featured blogs...</h1>
+            </main>
+        </div>
+    )
+}
+
+const HomePageLoading = () => {
+
+    return (
+        <div className="container">
+            <main className="home-main">
+                <MainArticlePreview isLoading={true} />
+            </main>
+            <div className="home-grid">
+                <ArticlePreview isLoading={true} />
+                <ArticlePreview isLoading={true} />
+                <ArticlePreview isLoading={true} />
+            </div>
+        </div>
+    )
+}
+
+const HomePage = () => {
+
+    // Gets featured blogs from useGetFeaturedBlogs hook
+
+    const { featuredBlogs, err } = useGetFeaturedBlogs();
+
+
+    // Checks if there was an error, and if featuredBlogs' length is greater than 1
+
+    if (err || (featuredBlogs && !featuredBlogs.length)) return <HomePageError />;
+    if (!featuredBlogs) return <HomePageLoading />;
+
+    const mainFeaturedBlog = featuredBlogs[0];
+
+    return (
+        <div className="container">
+
+            <main className="home-main">
+                <MainArticlePreview key={mainFeaturedBlog._id} articleInfo={mainFeaturedBlog} />
+            </main>
+
+            <aside className="home-aside">
+
+            </aside>
+
+            <div className="home-grid">
+
+                {featuredBlogs.map((featuredBlog: Article, index: number) => {
+
+                    if (index === 0) return;
+                    return <ArticlePreview key={featuredBlog._id} articleInfo={featuredBlog} />;
+
+                })}
+
+            </div>
+
+        </div>
     )
 }
 
