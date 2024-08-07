95e1865e5e4dbf449257025c6c3083f6d9dab031
Brandon Martin <SomeBusinessEmail@protonmail.com>
2024-08-07 12:19:08 -0400

    Made button take in handleClick method

--- src/components/EditButton/index.tsx
+++ src/components/EditButton/index.tsx
@@ -1,31 +1,17 @@
 // -- == [[ IMPORTS ]] == -- \\
 
-import { useNavigate } from "react-router-dom";
 import { FaPencilAlt } from "react-icons/fa";
 
 import './editButton.css';
-import { useLoggedInUser } from "@contexts/useLoggedInUser";
 
 
 // -- == [[ COMPONENTS ]] == -- \\
 
 type EditButtonProps = {
-    blog_id: string;
-    author_id: string;
+    handleClick: () => any;
 }
 
-const EditButton = ({ blog_id, author_id }: EditButtonProps) => {
-
-    const navigate = useNavigate();
-
-    const user = useLoggedInUser();
-    if (!user) return;
-    if (user._id !== author_id) return;
-
-    const handleClick = () => {
-        navigate(`/edit/blog/${blog_id}`);
-    }
-
+const EditButton = ({ handleClick }: EditButtonProps) => {
     return (
         <div className="edit" onClick={handleClick}>
             <FaPencilAlt className="pencil" />
